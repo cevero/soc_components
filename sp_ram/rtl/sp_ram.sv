29c525a2ce4aa0c6fb2f258b45f8d8e1f7b45ed6
Diego Cirilo <dvcirilo@gmail.com>
2019-04-29 15:33:02 -0300

    fixing bin paths

--- sp_ram/rtl/sp_ram.sv
+++ sp_ram/rtl/sp_ram.sv
@@ -20,7 +20,7 @@ module sp_ram
     initial begin
         for(int i = 0; i != 255; i = i + 1)
             mem[i] = 32'bx;
-        $readmemb("../soc_utils/fibonacci.bin", mem);
+        $readmemb("../../soc_utils/fibonacci.bin", mem);
     end
       
     always_ff @(posedge clk)
