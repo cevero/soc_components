fee169aac8ef674d9fc58ddbcdcb1028eb3f4dad
Diego Cirilo <dvcirilo@gmail.com>
2019-05-02 15:24:59 -0300

    [WIP] renaming signals

--- sp_ram/rtl/sp_ram.sv
+++ sp_ram/rtl/sp_ram.sv
@@ -7,19 +7,15 @@ module sp_ram
     input  logic                    clk,
     input  logic                    rst_n,
 
-    input  logic                    port_req_i,
-    input  logic [ADDR_WIDTH-1:0]   port_addr_i,
-    input  logic                    port_we_i,
-    input  logic [DATA_WIDTH-1:0]   port_wdata_i,
-    input  logic                    en_i,
+    input  logic                    req_i,
+    input  logic [ADDR_WIDTH-1:0]   addr_i,
+    input  logic                    we_i,
+    input  logic [DATA_WIDTH-1:0]   wdata_i,
     input  logic [DATA_WIDTH/8-1:0] be_i,
 
-    output logic                    port_gnt_o,
-    output logic                    port_rvalid_o,
-    output logic [DATA_WIDTH-1:0]   port_rdata_o,
-
-    output logic                    mem_flag,
-    output logic                    mem_result
+    output logic                    gnt_o,
+    output logic                    rvalid_o,
+    output logic [DATA_WIDTH-1:0]   rdata_o
 );
 
     //localparam words = NUM_WORDS/(DATA_WIDTH/8);
@@ -31,22 +27,21 @@ module sp_ram
 
     integer i;
 
-    assign addr = port_addr_i[ADDR_WIDTH-1:$clog2(DATA_WIDTH/8)];
+    assign addr = addr_i[ADDR_WIDTH-1:$clog2(DATA_WIDTH/8)];
 
     always @(posedge clk) begin
-        if (en_i && port_we_i) begin
+        if (rst_n && we_i) begin
             for (i = 0; i < DATA_WIDTH/8; i++) begin
             if (be_i[i])
               mem[addr][i] <= wdata[i];
             end
         end
-
-        port_rdata_o <= mem[addr];
+        rdata_o <= mem[addr];
     end
 
     genvar w;
     generate for(w = 0; w < DATA_WIDTH/8; w++) begin
-        assign wdata[w] = port_wdata_i[(w+1)*8-1:w*8];
+        assign wdata[w] = wdata_i[(w+1)*8-1:w*8];
     end
     endgenerate
 
